04f52d977ace42728355721958d25a4b4d20f5e9
Matthew <matthew41305@gmail.com>
2024-11-06 22:01:34 -0800

    added arrays with elements and output the numbers first and last, then pushed 5 values to the array

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-let myGrades = [95, 100, 76, 100, 85];
+// instantianted new array and added 5 numeric values
+let myAim = [10, 20, 30, 40, 50];
+
+// output first value of array
+console.log("First Value of array:", numbers[10]);
+
+// output the last value of the array
+console.log("Last Value of array:", numbers[50]);
+
+// The numbers that would have to get passed as indexes would be all of
+// them as they are split up with the values 0-4, we call these values by using console log since arrays
+
+console.log("Values within the array");
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+// pushing number
+numbers.push(69, 17, 99, 42, 34);
+
+// output contents
+
+
+
